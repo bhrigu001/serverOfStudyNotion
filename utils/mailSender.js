62a3b59f738b9381604343b05273de4156cc023f
bhrigu001 <mishrabhrigu2003@gmail.com>
2024-05-21 17:16:58 +0530

    Changed a spelling  mistake in mailSender file

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -2,7 +2,7 @@ const nodemailer=require("nodemailer");
 
 const mailSender = async(email,title,body)=>{
     try{
-        let transporter=nodemailer.createTrasnport({
+        let transporter=nodemailer.createTransport({
             host:process.env.MAIL_HOST,
             auth:{
                 user:process.env.MAIL_USER,
